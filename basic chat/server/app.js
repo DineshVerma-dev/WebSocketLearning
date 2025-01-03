0cae13c46b3fc37ceccad7893eec7904c76ece4d
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2025-01-03 12:13:57 +0530

    added bsic

--- basic chat/server/app.js
+++ basic chat/server/app.js
@@ -1,18 +1,40 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server)
 
-app.get("/", (req, res) => {
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+})
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+
+app.get("/", (_, res) => {
     res.send("hello world")
 })
+
 io.on("connection", (socket) => {
     console.log("User connected");
     console.log("Id : ", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log(`User disconnnected ${socket.id}`)
+    })
+
 })
-app.listen(4000, () => {
-    console.log("app started at port : 4000 ")
+
+
+server.listen(4000, () => {
+    console.log(`Server is running on port ${4000}`);
 })
